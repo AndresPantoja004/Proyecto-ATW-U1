a28b039c9885ce54742a81e972d87742acbe4508
Giss1106 <132688678+Giss1106@users.noreply.github.com>
2025-03-05 23:17:32 -0500

    arreglo10

--- detalleProducto.php
+++ detalleProducto.php
@@ -159,9 +159,9 @@
               Temporadas
             </a>
             <ul class="dropdown-menu" >
-              <li><a class="dropdown-item" href="#">Temporada 24-25</a></li>
-              <li><a class="dropdown-item" href="#">Temporada 23-24</a></li>                 
-              <li><a class="dropdown-item" href="#">Temporada 22-23</a></li>
+              <li><a class="dropdown-item" href="#">Temporadass 24-25</a></li>
+              <li><a class="dropdown-item" href="#">Temporadass 23-24</a></li>                 
+              <li><a class="dropdown-item" href="#">Temporadasss 22-23</a></li>
             </ul>
           </li>
         </ul>
